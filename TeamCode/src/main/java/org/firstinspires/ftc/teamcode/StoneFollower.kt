00ec4fe412d49a01d032519e6c2c2cba4b26569b
Razvan <razvan.filea@gmail.com>
2019-10-13 13:10:23 +0300

    Add some documentation and cleanup the StoneFollower code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StoneFollower.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StoneFollower.kt
@@ -2,13 +2,10 @@ package org.firstinspires.ftc.teamcode
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import com.qualcomm.robotcore.hardware.DcMotor
-import com.qualcomm.robotcore.hardware.DcMotorEx
 import com.qualcomm.robotcore.hardware.DcMotorSimple
 import org.firstinspires.ftc.robotcore.external.tfod.Recognition
 import java.util.concurrent.Executors
-import kotlin.time.ExperimentalTime
 
 @Autonomous(name = "StoneFollower")
 class StoneFollower : OpMode() {
@@ -18,60 +15,54 @@ class StoneFollower : OpMode() {
         const val HEIGHT_COEFFICIENT = 20 * 271
         const val TICKS_PER_CM = 35.6507
 
-        fun getTicksFromCM(x: Float): Double = x * TICKS_PER_CM
+        /*
+         * Transforms CM into encoder ticks for the Hex HD 1:40 motor
+         */
+        fun getTicksFromCM(cm: Float): Double = cm * TICKS_PER_CM
 
         fun getDist(height: Float) = HEIGHT_COEFFICIENT / height
 
-        fun isPositionEqual(first: Int, second: Int) = first / 10 == second / 10
+        /*
+         * Compares two encoder positions with a margin of error of one digit
+         */
+        infix fun Int.isPositionEqual(other: Int) = this / 10 == other / 10
 
         fun mapToRange(inputStart: Double, inputEnd: Double, outputStart: Double, outputEnd: Double, input: Double) =
             outputStart + ((outputEnd - outputStart) / (inputEnd - inputStart)) * (input - inputStart)
     }
 
-    private var isActive = false
     private val robot = TeamRobot()
     private val executor = Executors.newSingleThreadExecutor()
     private var cube: Recognition? = null
 
     override fun init() {
-        robot.onInit(hardwareMap)
-        val wheels = robot.wheels
+        robot.init(hardwareMap)
+        robot.vuforia.startDetectorAsync(hardwareMap)
 
-        robot.vuforiaManager.startDetectorAsync(hardwareMap)
-        wheels.left.direction = DcMotorSimple.Direction.REVERSE
-
-        wheels.setModeAll(DcMotor.RunMode.STOP_AND_RESET_ENCODER)
-        wheels.setModeAll(DcMotor.RunMode.RUN_USING_ENCODER)
+        with(robot.wheels) {
+            left.direction = DcMotorSimple.Direction.REVERSE
+            setModeAll(DcMotor.RunMode.STOP_AND_RESET_ENCODER)
+            setModeAll(DcMotor.RunMode.RUN_USING_ENCODER)
+        }
     }
 
     override fun start() {
-        isActive = true
         executor.submit {
-            while (isActive) {
-                cube = robot.vuforiaManager.recognitions.firstOrNull { it.label == VuforiaManager.LABEL_GOLD_MINERAL }
-                cube?.let {
-                    telemetry.addData("Cube Height", it.height)
-                    telemetry.addData("Dist", getDist(it.height))
-                    telemetry.update()
-                }
-            }
+            while (robot.isOpModeActive)
+                cube = robot.vuforia.recognitions.firstOrNull { it.label == VuforiaManager.LABEL_GOLD_MINERAL }
             Thread.sleep(20)
         }
 
-        robot.vuforiaManager.waitForDetector()
+        robot.vuforia.waitForDetector()
     }
 
     override fun loop() {
-        val wheels = robot.wheels
         val cube = cube
 
         if (cube == null) {
-            wheels.setPowerAll(0.0)
+            robot.wheels.setPowerAll(0.0)
             return
         }
-
-        val leftPid = wheels.left as DcMotorEx
-        leftPid.velocity
 
         val cubePos = (cube.left + cube.width / 2) / cube.imageWidth
         val dist = getDist(cube.height)
@@ -85,36 +76,37 @@ class StoneFollower : OpMode() {
                 val rightPower = mapToRange(0.53, 1.0, 0.5, 1.0, cubePos.toDouble())
                 move(dist, DEFAULT_POWER, DEFAULT_POWER * rightPower)
             }
-            else -> move(dist, DEFAULT_POWER, DEFAULT_POWER)
+            else -> move(dist, DEFAULT_POWER, DEFAULT_POWER) // Middle
         }
     }
 
     override fun stop() {
-        isActive = false
+        robot.stop()
         executor.shutdown()
-        robot.vuforiaManager.stopCamera()
     }
 
     private fun move(dist: Float, leftPower: Double, rightPower: Double) {
         val wheels = robot.wheels
 
         val finalLeftPower = run {
-            val leftPos = wheels.left.currentPosition
-            val leftTarget = leftPos + getTicksFromCM(dist).toInt()
+            val currentPosition = wheels.left.currentPosition
+            val targetPosition = currentPosition + getTicksFromCM(dist).toInt()
 
-            if (!isPositionEqual(leftPos, leftTarget)) {
-                val sign = if (leftPos < leftTarget) 1 else -1
+            if (!(currentPosition isPositionEqual targetPosition)) { // The motor hasn't reached its destination
+                // Check if we need to go forwards or backwards
+                val sign = if (currentPosition < targetPosition) 1 else -1
 
                 leftPower * sign
             } else 0.0
         }
 
         val finalRightPower = run {
-            val rightPos = wheels.right.currentPosition
-            val rightTarget = rightPos + getTicksFromCM(dist).toInt()
+            val currentPosition = wheels.right.currentPosition
+            val targetPosition = currentPosition + getTicksFromCM(dist).toInt()
 
-            if (!isPositionEqual(rightPos, rightTarget)) {
-                val sign = if (rightPos < rightTarget) 1 else -1
+            if (!(currentPosition isPositionEqual targetPosition)) { // The motor hasn't reached its destination
+                // Check if we need to go forwards or backwards
+                val sign = if (currentPosition < targetPosition) 1 else -1
 
                 rightPower * sign
             } else 0.0
@@ -126,5 +118,6 @@ class StoneFollower : OpMode() {
         telemetry.addData("Left", wheels.left.currentPosition)
         telemetry.addData("Right", wheels.right.currentPosition)
         telemetry.addData("Distance", getTicksFromCM(dist))
+        telemetry.update()
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamRobot.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamRobot.kt
@@ -8,15 +8,36 @@ class TeamRobot {
     var x = 0
     var y = 0
     var isOpModeActive = false
+        private set
     var wheels: WheelMotors by Delegates.notNull()
         private set
+    val vuforia = VuforiaManager()
 
-    val vuforiaManager = VuforiaManager()
-
-    fun onInit(hardwareMap: HardwareMap) {
+    fun init(hardwareMap: HardwareMap) {
         x = 0
         y = 0
-        isOpModeActive = false
+        isOpModeActive = true
         wheels = WheelMotors(hardwareMap.dcMotor)
+
+        INSTANCE = this
+    }
+
+    fun stop() {
+        INSTANCE = null
+
+        isOpModeActive = false
+        vuforia.stopCamera()
+    }
+
+    companion object {
+        private var INSTANCE: TeamRobot? = null
+
+        /*
+         * This function should only be called after TeamRobot::init has been called
+         * and before TeamRobot::stop was called
+         *
+         * If it is called at any other time, the function will throw an NPE
+         */
+        fun getRobot(): TeamRobot = INSTANCE!!
     }
 }
