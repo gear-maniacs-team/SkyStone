c90d1806192b364345d9dd8573c718c8ba151b1a
Gear Maniacs <gearmaniacsteam@gmail.com>
2019-11-19 15:45:49 +0200

    Create the Gyro class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/TestingIMU.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/TestingIMU.kt
@@ -8,19 +8,18 @@ import org.firstinspires.ftc.teamcode.utils.fastLazy
 import java.lang.Thread.sleep
 
 @TeleOp(name = "TestingIMU")
-class TestingIMU :OpMode() {
+class TestingIMU : OpMode() {
     private val robot = TeamRobot()
     private val wheelMotors by fastLazy { robot.wheelsMotors }
     private lateinit var imu: BNO055IMU
 
     override fun init() {
-        val parameters = BNO055IMU.Parameters()
-
-        parameters.mode = BNO055IMU.SensorMode.IMU
-        parameters.angleUnit = BNO055IMU.AngleUnit.RADIANS
-        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC
-        parameters.loggingEnabled = false
-
+        val parameters = BNO055IMU.Parameters().apply {
+            mode = BNO055IMU.SensorMode.IMU
+            angleUnit = BNO055IMU.AngleUnit.RADIANS
+            accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC
+            loggingEnabled = false
+        }
 
         imu = hardwareMap.get(BNO055IMU::class.java, "imu")
 
@@ -29,9 +28,8 @@ class TestingIMU :OpMode() {
         telemetry.update();
 
         // make sure the imu gyro is calibrated before continuing.
-        while (!imu.isGyroCalibrated)
-        {
-            sleep(50);
+        while (!imu.isGyroCalibrated) {
+            sleep(10);
         }
         telemetry.addData("Mode", "waiting for start");
         telemetry.addData("imu calibration status", imu.calibrationStatus.toString());
@@ -39,8 +37,7 @@ class TestingIMU :OpMode() {
     }
 
     override fun loop() {
-        telemetry.addData("Angle",imu.angularOrientation)
+        telemetry.addData("Angle", imu.angularOrientation)
         telemetry.update()
     }
-
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sensors/Gyro.kt
@@ -0,0 +1,60 @@
+package org.firstinspires.ftc.teamcode.sensors
+
+import com.qualcomm.hardware.bosch.BNO055IMU
+import com.qualcomm.robotcore.hardware.HardwareMap
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference
+import org.firstinspires.ftc.teamcode.TeamRobot
+
+class Gyro {
+
+    private val reference = AxesReference.EXTRINSIC
+    private val angleOrder = AxesOrder.XYZ
+    private val angleUnit = AngleUnit.RADIANS
+
+    private lateinit var firstImu: BNO055IMU
+    private lateinit var secondImu: BNO055IMU
+    var angle = 0.0
+        private set
+
+    fun init(hardwareMap: HardwareMap) {
+        firstImu = hardwareMap.get(BNO055IMU::class.java, "imu_1")
+        secondImu = hardwareMap.get(BNO055IMU::class.java, "imu_2")
+
+        val parameters = BNO055IMU.Parameters().apply {
+            mode = BNO055IMU.SensorMode.IMU
+            angleUnit = BNO055IMU.AngleUnit.RADIANS
+            accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC
+            loggingEnabled = false
+        }
+
+        firstImu.initialize(parameters)
+        secondImu.initialize(parameters)
+    }
+
+    fun waitForCalibration() {
+        while (!firstImu.isGyroCalibrated || !secondImu.isGyroCalibrated) {
+            Thread.sleep(10);
+        }
+    }
+
+    private fun updateValue() {
+        val firstAngle = firstImu.getAngularOrientation(reference, angleOrder, angleUnit).thirdAngle.toDouble()
+        val secondAngle = secondImu.getAngularOrientation(reference, angleOrder, angleUnit).thirdAngle.toDouble()
+
+        angle = (firstAngle + secondAngle) / 2
+    }
+
+    fun start() {
+        waitForCalibration()
+
+        val robot = TeamRobot.getRobot()
+        Thread {
+            while (robot.isOpModeActive) {
+                updateValue()
+                Thread.sleep(100)
+            }
+        }.start()
+    }
+}
