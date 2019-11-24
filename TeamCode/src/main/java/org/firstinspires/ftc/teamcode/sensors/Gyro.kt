2c4c5d0cfbffeba055c458b88479e6b620624ae3
Razvan <razvan.filea@gmail.com>
2019-11-24 21:24:43 +0200

    Set debuggable attribute to false

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sensors/Gyro.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sensors/Gyro.kt
@@ -35,7 +35,7 @@ class Gyro {
 
     fun waitForCalibration() {
         while (!firstImu.isGyroCalibrated || !secondImu.isGyroCalibrated) {
-            Thread.sleep(10);
+            Thread.sleep(10)
         }
     }
 
