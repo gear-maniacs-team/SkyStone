c8e2d3ac333627b622027890cf091f2a7e1b5927
Razvan <razvan.filea@gmail.com>
2020-02-09 17:43:15 +0200

    Add AnyMark3637 Coefficients and RobotPos loading/saving

--- TeamCode/src/main/java/net/gearmaniacs/teamcode/hardware/motors/type/AndyMark3637.kt
+++ TeamCode/src/main/java/net/gearmaniacs/teamcode/hardware/motors/type/AndyMark3637.kt
@@ -1,8 +1,9 @@
 package net.gearmaniacs.teamcode.hardware.motors.type
 
 import com.qualcomm.robotcore.hardware.configuration.DistributorInfo
-import com.qualcomm.robotcore.hardware.configuration.ModernRoboticsMotorControllerParams
 import com.qualcomm.robotcore.hardware.configuration.annotations.DeviceProperties
+import com.qualcomm.robotcore.hardware.configuration.annotations.ExpansionHubPIDFPositionParams
+import com.qualcomm.robotcore.hardware.configuration.annotations.ExpansionHubPIDFVelocityParams
 import com.qualcomm.robotcore.hardware.configuration.annotations.MotorType
 import org.firstinspires.ftc.robotcore.external.navigation.Rotation
 
@@ -14,5 +15,17 @@ import org.firstinspires.ftc.robotcore.external.navigation.Rotation
     model = "am-3637",
     url = "https://www.andymark.com/products/neverest-orbital-20-gearmotor"
 )
-@ModernRoboticsMotorControllerParams(P = 160, I = 32, D = 112, ratio = 25)
+/**
+ * Please note that these Velocity PIDF Coefficients are calculated from our robot's
+ * specific max velocity and may not work as well for you
+ *
+ * How these values were calculated:
+ * * Find the max motor velocity (maxV) in ticks per second
+ * * Calculate F: F = 32767 / maxV
+ * * Calculate P: P = F / 10
+ * * Calculate I: I = P / 10
+ * * D remains 0
+ */
+@ExpansionHubPIDFVelocityParams(P = 1.32, I = 0.132, D = 0.0, F = 13.2)
+@ExpansionHubPIDFPositionParams(P = 5.0)
 interface AndyMark3637
