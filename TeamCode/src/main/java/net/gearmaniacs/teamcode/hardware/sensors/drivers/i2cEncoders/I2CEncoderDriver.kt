0c360de8e0124bcf81ead1cf6955dc88b35e4f26
2Look <david.lucian.oancea@gmail.com>
2020-02-28 21:58:42 +0200

    Complete I2CEncoder driver

--- TeamCode/src/main/java/net/gearmaniacs/teamcode/hardware/sensors/drivers/i2cEncoders/I2CEncoderDriver.kt
+++ TeamCode/src/main/java/net/gearmaniacs/teamcode/hardware/sensors/drivers/i2cEncoders/I2CEncoderDriver.kt
@@ -5,10 +5,11 @@ import com.qualcomm.robotcore.hardware.I2cDeviceSynch
 import com.qualcomm.robotcore.hardware.I2cDeviceSynchDevice
 import com.qualcomm.robotcore.hardware.configuration.annotations.DeviceProperties
 import com.qualcomm.robotcore.hardware.configuration.annotations.I2cDeviceType
+import java.nio.ByteBuffer
 
 @I2cDeviceType
-@DeviceProperties(name = "Encoder Sensor", xmlTag = "EncoderSensor")
-class EncoderDI2CDriver(deviceClient: I2cDeviceSynch) : I2cDeviceSynchDevice<I2cDeviceSynch>(deviceClient, true), II2CInterface {
+@DeviceProperties(name = "I2C Encoder Sensor", description = "A loophole in the game manual being exploited", xmlTag = "EncoderSensor")
+class I2CEncoderDriver(deviceClient: I2cDeviceSynch) : I2cDeviceSynchDevice<I2cDeviceSynch>(deviceClient, true), II2CInterface {
 
     private val encoder = I2CEncoderV2(this)
 
@@ -17,12 +18,15 @@ class EncoderDI2CDriver(deviceClient: I2cDeviceSynch) : I2cDeviceSynchDevice<I2c
 
     }
 
+    val currentPosition
+        get() = encoder.readCounter()
+
     override fun doInitialize(): Boolean {
         return true
     }
 
     override fun getDeviceName(): String {
-        return ""
+        return "I2C Encoder"
     }
 
     override fun getManufacturer(): HardwareDevice.Manufacturer {
@@ -30,18 +34,20 @@ class EncoderDI2CDriver(deviceClient: I2cDeviceSynch) : I2cDeviceSynchDevice<I2c
     }
 
     override fun write8(address: Int, bValue: Int): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        deviceClient.write8(address,bValue)
+        return true
     }
 
     override fun read8(address: Int): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return deviceClient.read8(address).toInt()
     }
 
     override fun read32(address: Int): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return ByteBuffer.wrap(deviceClient.read(address,4)).int
     }
 
     override fun write32(address: Int, value: Int): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        deviceClient.write(address,ByteBuffer.allocate(4).putInt(value).array())
+        return true
     }
 }
