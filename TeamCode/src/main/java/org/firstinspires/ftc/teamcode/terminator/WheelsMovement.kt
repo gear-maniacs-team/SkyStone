08697f4ff6a488295e700ca01aeeff00ab13ac81
Bogdan <bobo.oprisiu@gmail.com>
2020-07-17 11:46:04 +0300

    Updated Movement

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/terminator/WheelsMovement.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/terminator/WheelsMovement.kt
@@ -1,38 +1,57 @@
 package org.firstinspires.ftc.teamcode.terminator
 
+import com.acmerobotics.dashboard.FtcDashboard
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import net.gearmaniacs.teamcode.RobotPos
+import net.gearmaniacs.teamcode.TeamRobot
+import net.gearmaniacs.teamcode.hardware.sensors.Encoders
 
 //adb connect 192.168.43.1
 @Autonomous(name = "Boogaloo", group = "Boogaloo")
 class FoundationRed : LinearOpMode() {
 
+    private val YPID = PIDController(0.02,0.0000005,4.2)
+    private val APID = PIDController(0.02,0.0000005,4.2)
+    private val XPID = PIDController(0.02,0.0000005,4.2)
+    private val robot = TeamRobot()
     private val wheels = Wheels()
+    private val encoders = Encoders()
 
     override fun runOpMode() {
+        RobotPos.resetAll()
+        robot.init(hardwareMap, listOf(encoders), listOf(encoders))
         wheels.init(hardwareMap)
+        robot.start()
         waitForStart()
+        YPID.setPoint = 60.0
+        APID.setPoint = 0.0
+        XPID.setPoint = 0.0
+        YPID.setOutputRange(-1.0, 1.0)
+        APID.setOutputRange(-1.0, 1.0)
+        XPID.setOutputRange(-1.0, 1.0)
 
-//        wheels.goRightForward(50.0,0.3)
-//        wheels.waitForMotors()
-//        Thread.sleep(1000)
+        while (opModeIsActive()){
+            val yResult = YPID.performPID(RobotPos.currentY)
+            val aResult = APID.performPID(Math.toDegrees(RobotPos.currentAngle))
+            val xResult = XPID.performPID(RobotPos.currentX)
 
-        wheels.goRightCm(50.0,0.3)
-        wheels.waitForMotors()
-        Thread.sleep(1000)
-//
-//        wheels.goForwardCm(100.0,0.3)
-//        wheels.waitForMotors()
-//        Thread.sleep(1000)
-//
-//        wheels.goLeftCm(50.0,0.3)
-//        wheels.waitForMotors()
-//        Thread.sleep(1000)
-//
-//        wheels.goBackCm(50.0,0.3)
-//        wheels.waitForMotors()
-//        Thread.sleep(1000)
+            wheels.frontLeft.power = yResult + aResult + xResult
+            wheels.frontRight.power = yResult - aResult - xResult
+            wheels.backLeft.power = yResult + aResult - xResult
+            wheels.backRight.power = yResult - aResult + xResult
 
-        //while(OpModeIsActive)
+            val packet = TelemetryPacket().apply {
+                put("XPos", RobotPos.currentX)
+                put("YPos", RobotPos.currentY)
+                put("Angle", Math.toDegrees(RobotPos.currentAngle))
+            }
+            FtcDashboard.getInstance().sendTelemetryPacket(packet)
+        }
+
+        robot.stop()
     }
+
+
 }
