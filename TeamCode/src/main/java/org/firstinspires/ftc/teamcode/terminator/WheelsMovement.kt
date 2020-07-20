0421941e5937ccde8a6986314d15ba794ec1f6c5
Bogdan <bobo.oprisiu@gmail.com>
2020-07-20 11:32:32 +0300

    Magic PID

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/terminator/WheelsMovement.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/terminator/WheelsMovement.kt
@@ -7,14 +7,16 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import net.gearmaniacs.teamcode.RobotPos
 import net.gearmaniacs.teamcode.TeamRobot
 import net.gearmaniacs.teamcode.hardware.sensors.Encoders
+import net.gearmaniacs.teamcode.utils.RobotClock
 
 //adb connect 192.168.43.1
 @Autonomous(name = "Boogaloo", group = "Boogaloo")
 class FoundationRed : LinearOpMode() {
 
-    private val YPID = PIDController(0.02,0.0000005,4.2)
-    private val APID = PIDController(0.02,0.0000005,4.2)
-    private val XPID = PIDController(0.02,0.0000005,4.2)
+
+    private val YPID = PIDController(0.04,0.000005,4.0)
+    private val APID = PIDController(0.04,0.000005,4.0)//69420
+    private val XPID = PIDController(0.05,0.000005,4.0)
     private val robot = TeamRobot()
     private val wheels = Wheels()
     private val encoders = Encoders()
@@ -25,14 +27,15 @@ class FoundationRed : LinearOpMode() {
         wheels.init(hardwareMap)
         robot.start()
         waitForStart()
-        YPID.setPoint = 60.0
-        APID.setPoint = 0.0
-        XPID.setPoint = 0.0
+        YPID.setPoint = 45.0
+        APID.setPoint = 90.0
+        XPID.setPoint = 45.0
         YPID.setOutputRange(-1.0, 1.0)
         APID.setOutputRange(-1.0, 1.0)
         XPID.setOutputRange(-1.0, 1.0)
 
         while (opModeIsActive()){
+
             val yResult = YPID.performPID(RobotPos.currentY)
             val aResult = APID.performPID(Math.toDegrees(RobotPos.currentAngle))
             val xResult = XPID.performPID(RobotPos.currentX)
